9654c03f95c31f7458a21476cea85475fc6e437f
dhananjaybhatia <dhananjaybhatia@yahoo.co.in>
2025-08-16 14:17:23 +1000

    implement components card design

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,42 @@
-import { Button } from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionList from "@/components/CompanionList";
+import CTA from "@/components/CTA";
 
 const page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my Saas</h1>
-      <Button>Click</Button>
-    </div>
+    <main>
+      <h1 className="text-2xl">Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          duration={45}
+          subject="Science"
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          duration={35}
+          subject="Maths"
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Verbal Vocabulary Builder"
+          topic="English Literature"
+          duration={30}
+          subject="Language"
+          color="#BDE7FF"
+        />
+      </section>
+      <section className="home-section">
+        <CompanionList />
+        <CTA />
+      </section>
+    </main>
   );
 };
 
