4cb5cbe6f9ea748978ec25121c73537470e55247
dhananjaybhatia <dhananjaybhatia@yahoo.co.in>
2025-08-16 13:32:01 +1000

    implement Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { Button } from "@/components/ui/button";
 const page = () => {
   return (
     <div>
-      <h1 className="text-2xl underline">hey</h1>
-      <Button> click</Button>
+      <h1 className="text-2xl underline">Welcome to my Saas</h1>
+      <Button>Click</Button>
     </div>
   );
 };
