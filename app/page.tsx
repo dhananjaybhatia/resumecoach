8d396e8a6e6d305f7783adda6e2681c48a98be88
dhananjaybhatia <dhananjaybhatia@yahoo.co.in>
2025-08-16 13:01:58 +1000

    routes created

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 const page = () => {
   return (
     <div>
       <h1 className="text-2xl underline">hey</h1>
+      <Button> click</Button>
     </div>
   );
 };
